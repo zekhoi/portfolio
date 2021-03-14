5c94fae753b91fc7ce99e257c539aa7b9cff068d
Khoironi Kurnia Syah <khoironidev@gmail.com>
2021-03-14 21:21:48 +0800

    Update border style

--- components/homepage/Activities.js
+++ components/homepage/Activities.js
@@ -8,7 +8,7 @@ export default function Activities() {
                     <h3 className="text-xl text-center md:text-2xl font-medium mb-2 py-2">Activities</h3>
                     <div className="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 mx-auto">
                         <div className="max-w rounded-lg hover:shadow-lg shadow transform transition duration-500 hover:scale-110 m-2">
-                            <img className="object-cover w-full border rounded" src="https://careers.shopee.sg/codeleague/images/seo.jpg" alt="Shopee Code League 2021"/>
+                            <img className="object-cover w-full border-t border-b rounded-b-none rounded" src="https://careers.shopee.sg/codeleague/images/seo.jpg" alt="Shopee Code League 2021"/>
                             <div className="px-6 py-4">
                                 <h4 className="mb-3 text-xl font-semibold tracking-tight text-gray-800">Shopee Code League 2021</h4>
                                 <Link href="https://github.com/zekhoi/shopeecodeleague">
