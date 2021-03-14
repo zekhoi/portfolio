83816f704af31c5a538741747e0ec6a79c00d7a7
Khoironi Kurnia Syah <khoironidev@gmail.com>
2021-03-14 21:15:24 +0800

    Update wrong image line

--- components/homepage/Activities.js
+++ components/homepage/Activities.js
@@ -4,22 +4,22 @@ export default function Activities() {
     return (
         <div>
             <div id="activities">
-            <div className="container px-8 mx-auto py-4">
-                <h3 className="text-xl text-center md:text-2xl font-medium mb-2 py-2">Activities</h3>
-                <div className="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 mx-auto">
-                <div className="max-w rounded-lg hover:shadow-lg shadow transform transition duration-500 hover:scale-110 m-2">
-                    <img className="object-cover w-full border rounded" src="https://careers.shopee.sg/codeleague/images/seo.jpg" alt="Shopee Code League 2021"/>
-                    <div className="px-6 py-4">
-                    <h4 className="mb-3 text-xl font-semibold tracking-tight text-gray-800">Shopee Code League 2021</h4>
-                    <Link href="https://github.com/zekhoi/shopeecodeleague">
-                        <button className="text-white bg-black border border-solid border-black hover:bg-white hover:text-black active:bg-white font-bold px-3 py-2 md:px-3 md:py-2 rounded outline-none focus:outline-none mr-1 mb-1" type="button" style={{ transition: "all .15s ease" }}>
-                            <i class="fab fa-github"></i> Get Code
-                        </button>
-                    </Link>
+                <div className="container px-8 mx-auto py-4">
+                    <h3 className="text-xl text-center md:text-2xl font-medium mb-2 py-2">Activities</h3>
+                    <div className="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 mx-auto">
+                        <div className="max-w rounded-lg hover:shadow-lg shadow transform transition duration-500 hover:scale-110 m-2">
+                            <img className="object-cover w-full border rounded" src="https://careers.shopee.sg/codeleague/images/seo.jpg" alt="Shopee Code League 2021"/>
+                            <div className="px-6 py-4">
+                                <h4 className="mb-3 text-xl font-semibold tracking-tight text-gray-800">Shopee Code League 2021</h4>
+                                <Link href="https://github.com/zekhoi/shopeecodeleague">
+                                    <button className="text-white bg-black border border-solid border-black hover:bg-white hover:text-black active:bg-white font-bold px-3 py-2 md:px-3 md:py-2 rounded outline-none focus:outline-none mr-1 mb-1" type="button" style={{ transition: "all .15s ease" }}>
+                                        <i class="fab fa-github"></i> Get Code
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-            </div>
             </div>
         </div>
     )
--- components/homepage/Projects.js
+++ components/homepage/Projects.js
@@ -8,16 +8,16 @@ export default function Projects() {
                     <h3 className="text-xl text-center md:text-2xl font-medium mb-2 py-2">Projects</h3>
                     {/* <h1 className="text-4xl text-center">Work In Progress</h1> */}
                     <div className="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-2 mx-auto ">
-                        <div className="max-w rounded-lg hover:shadow-lg transform transition duration-500 hover:scale-110 m-2">
+                        <div className="max-w rounded-lg shadow hover:shadow-lg transform transition duration-500 hover:scale-110 m-2">
+                            <img className="object-cover w-full border rounded" src="/images/projects/portfolio.png" alt="Next JS Portfolio Website"/>
                             <div className="px-6 py-4">
-                            <img className="object-cover w-full border" src="/images/projects/portfolio.png" alt="Next JS Portfolio Website"/>
-                            <h4 className="mb-3 text-xl font-semibold tracking-tight text-gray-800">Portfolio Website</h4>
-                            <h5 className="text-md font-semibold tracking-tight text-gray-800">Technologies :</h5>
+                                <h4 className="mb-3 text-xl font-semibold tracking-tight text-gray-800">Portfolio Website</h4>
+                                <h5 className="text-md font-semibold tracking-tight text-gray-800">Technologies :</h5>
                                 <div class="flex justify-start gap-1 py-2">
-                                <Image src="/images/icons/html5.png" alt="HTML5" width={25} height={25} />
-                                <Image src="/images/icons/css3.png" alt="CSS3" width={25} height={25} />
-                                <Image src="/images/icons/nextjs.png" alt="Next JS" width={25} height={25} />
-                                <Image src="/images/icons/tailwind.png" alt="Tailwind" width={25} height={25} />
+                                    <Image src="/images/icons/html5.png" alt="HTML5" width={25} height={25} />
+                                    <Image src="/images/icons/css3.png" alt="CSS3" width={25} height={25} />
+                                    <Image src="/images/icons/nextjs.png" alt="Next JS" width={25} height={25} />
+                                    <Image src="/images/icons/tailwind.png" alt="Tailwind" width={25} height={25} />
                                 </div>
                             </div>
                         </div>
