ce43e67a6f90f02aa17c75552421e2cccfdfaf8f
Khoironi Kurnia Syah <khoironidev@gmail.com>
2021-03-24 12:43:04 +0800

    add something

--- components/homepage/About.js
+++ components/homepage/About.js
@@ -13,9 +13,9 @@ export default function About() {
                         <div className="max-w-d h-auto p-4 my-auto">
                             <h2 className="text-2xl md:text-2xl font-medium py-2">About Me</h2>
                             <h3 className="font-sans font-medium text-xl text-gray-700">Full-Stack Developer</h3>
-                            <h5 className="font-sans mb-8 mt-4 text-gray-700">
+                            <h4 className="font-sans mb-8 mt-4 text-gray-700">
                             Responsible for front-end and back-end web development. Understand several how to work with several languages and databases including PHP, HTML, CSS, JavaScript and everything in between.
-                            </h5>
+                            </h4>
                             <Link href="https://www.linkedin.com/in/khoironiks/">
                                 <button className="text-white bg-black border border-solid border-black hover:bg-white hover:text-black active:bg-white font-bold px-3 py-2 md:px-3 md:py-2 rounded outline-none focus:outline-none mr-1 mb-1" type="button" style={{ transition: "all .15s ease" }}>
                                     Contact Me
--- components/homepage/Main.js
+++ components/homepage/Main.js
@@ -11,7 +11,7 @@ export default function Main() {
                             <div className="text-center md:text-left my-auto">
                                 <h5 className="text-md md:text-xl font-bold">Hello, I am</h5>
                                 <h1 className="text-2xl md:text-3xl lg:text-5xl font-medium uppercase my-2">Khoironi Kurnia Syah</h1>
-                                <h3 className="text-xl md:text-2xl font-medium mb-4">Full-Stack Developer</h3>
+                                <h2 className="text-xl md:text-2xl font-medium mb-4">Full-Stack Developer</h2>
                                 <Link href="https://www.linkedin.com/in/khoironiks/">
                                     <button className="text-white border border-solid bg-black border-black hover:bg-white hover:text-black active:bg-white font-bold uppercase px-3 py-2 md:px-5 md:py-2 rounded outline-none focus:outline-none mr-1 mb-1" type="button" style={{ transition: "all .15s ease" }}>
                                         Hire Me
