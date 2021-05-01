22e814797da3adda6e90549d7e07357903015db8
Khoironi Kurnia Syah <khoironidev@gmail.com>
2021-05-01 23:36:07 +0800

    add skill logo

--- components/homepage/Skills.js
+++ components/homepage/Skills.js
@@ -11,7 +11,7 @@ export default function Skills() {
                     <h3 className="text-xl text-center md:text-2xl font-medium py-2 mt-14">Skills</h3>
                     </Fade>
 
-                    <div className="grid xl:grid-cols-6 md:grid-cols-4 grid-cols-3 gap-6 text-center">
+                    <div className="grid xl:grid-cols-8 md:grid-cols-4 grid-cols-4 text-center">
 
                         <Fade bottom>
                         <div className="max-w-d h-auto rounded-lg transform transition duration-500 hover:scale-110 p-4">
@@ -45,6 +45,12 @@ export default function Skills() {
 
                         <Fade bottom>
                         <div className="max-w-d h-auto rounded-lg transform transition duration-500 hover:scale-110 p-4">
+                            <Image src="/images/icons/dart.png" alt="Dart" width={50} height={50} />
+                        </div>
+                        </Fade>
+
+                        <Fade bottom>
+                        <div className="max-w-d h-auto rounded-lg transform transition duration-500 hover:scale-110 p-4">
                             <Image src="/images/icons/postgresql.png" alt="PostgreSQL" width={50} height={50} />
                         </div>
                         </Fade>
@@ -58,6 +64,18 @@ export default function Skills() {
                         <Fade bottom>
                         <div className="max-w-d h-auto rounded-lg transform transition duration-500 hover:scale-110 p-4">
                             <Image src="/images/icons/nextjs.png" alt="Next JS" width={50} height={50} />
+                        </div>
+                        </Fade>
+
+                        <Fade bottom>
+                        <div className="max-w-d h-auto rounded-lg transform transition duration-500 hover:scale-110 p-4">
+                            <Image src="/images/icons/react.png" alt="React JS" width={50} height={50} />
+                        </div>
+                        </Fade>
+
+                        <Fade bottom>
+                        <div className="max-w-d h-auto rounded-lg transform transition duration-500 hover:scale-110 p-4">
+                            <Image src="/images/icons/flutter.png" alt="Flutter" width={50} height={50} />
                         </div>
                         </Fade>
 
@@ -82,6 +100,12 @@ export default function Skills() {
                         <Fade bottom>
                         <div className="max-w-d h-auto rounded-lg transform transition duration-500 hover:scale-110 p-4">
                             <Image src="/images/icons/github.png" alt="Github" width={50} height={50} />
+                        </div>
+                        </Fade>
+
+                        <Fade bottom>
+                        <div className="max-w-d h-auto rounded-lg transform transition duration-500 hover:scale-110 p-4">
+                            <Image src="/images/icons/docker.png" alt="Docker" width={50} height={50} />
                         </div>
                         </Fade>
 
