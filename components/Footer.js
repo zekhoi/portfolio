04f486726267a71b3559c4e0acfaaa02b4ee2e58
Khoironi Kurnia Syah <khoironidev@gmail.com>
2021-05-02 09:19:38 +0800

    fix

--- components/Footer.js
+++ components/Footer.js
@@ -28,19 +28,21 @@ export default function Footer() {
 
     return (
         <div>
-            <div className="container mx-auto px-6">
-                <div className="border-gray-300 flex flex-col items-center">
-                    <div className="sm:w-2/3 text-center py-6">
-                        <Fade bottom>
-                        <p className="text-sm text-black font-medium mb-2">
-                            © 2021 Made with <i className="lni lni-heart"></i> by Khoironi Kurnia Syah
-                        </p>
-                        </Fade>
+            <div className="bg-two">
+                <div className="container mx-auto px-6">
+                    <div className="border-gray-300 flex flex-col items-center">
+                        <div className="sm:w-2/3 text-center py-2">
+                            <Fade bottom>
+                            <p className="text-sm text-white font-medium">
+                                © 2021 Made with <i className="lni lni-heart"></i> by Khoironi Kurnia Syah
+                            </p>
+                            </Fade>
+                        </div>
                     </div>
+                    <Fade right>
+                        <button className={`${over ? "hidden" : ""} fixed rounded-full bg-two text-white shadow-md p-3 flex right-5 bottom-5 z-50 focus:outline-none`} onClick={() => scroll.scrollToTop()}><i className="lni lni-chevron-up"></i></button>
+                    </Fade>
                 </div>
-                <Fade right>
-                    <button className={`${over ? "hidden" : ""} fixed rounded-full bg-two text-white p-3 flex right-5 bottom-5 z-50 focus:outline-none`} onClick={() => scroll.scrollToTop()}><i className="lni lni-chevron-up"></i></button>
-                </Fade>
             </div>
         </div>
     )
